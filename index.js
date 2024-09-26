9762a3cd8d374df504d5eb7eef98215dd91ca6e1
Sirawit Pulketkij <sirawit.pulketkij@gmail.com>
2024-09-26 22:05:35 +0700

    add influxdb

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require("http");
 const socketIo = require("socket.io");
 const cors = require("cors");
 require("dotenv").config();
+const { InfluxDBClient, Point } = require("@influxdata/influxdb3-client");
 
 const app = express();
 const server = http.createServer(app);
@@ -49,6 +50,63 @@ mqtt_client.on("message", function (topic, message) {
 
 // subscribe to topic 'my/test/topic'
 mqtt_client.subscribe("my/test/topic");
+
+// สร้าง InfluxDB client โดยใช้ environment variables
+const client = new InfluxDBClient({
+  host: "https://us-east-1-1.aws.cloud2.influxdata.com",
+  token:
+    "jpMFNwx0dwjPpOoTXfhYB2RmcaN6z9oGqGqupOI2gDMEm55dgSXJSaiHND-2RC2VBRmjv2uliXg1teuwHXjJrw==",
+});
+
+async function uploadGyroscope(location, x, y, z) {
+  const gyroscopePoint = Point.measurement("gyroscope")
+    .setTag("location", location)
+    .setFloatField("x", x)
+    .setFloatField("y", y)
+    .setFloatField("z", z)
+    .setTimestamp(new Date());
+
+  await client.write(gyroscopePoint, "wireless");
+}
+
+async function uploadGPS(location, latitude, longitude, altitude) {
+  const gpsPoint = Point.measurement("gps")
+    .setTag("location", location)
+    .setFloatField("latitude", latitude)
+    .setFloatField("longitude", longitude)
+    .setFloatField("altitude", altitude)
+    .setTimestamp(new Date());
+
+  await client.write(gpsPoint, "wireless");
+}
+
+/**
+ * ฟังก์ชันปิดการเชื่อมต่อ
+ */
+async function closeConnection() {
+  try {
+    await client.close();
+    console.log("Data uploaded successfully!");
+  } catch (err) {
+    console.error("Error uploading data: ", err);
+  }
+}
+
+// ตัวอย่างการอัพโหลดข้อมูลหลายรอบ
+async function uploadData() {
+  // อัพโหลดข้อมูล Gyroscope
+  uploadGyroscope("office", 0.12, -0.34, 1.01);
+  uploadGyroscope("office", 0.15, -0.3, 1.02); // อัพโหลดรอบที่สอง
+
+  // อัพโหลดข้อมูล GPS
+  uploadGPS("car", 13.7563, 100.5018, 5.2);
+  uploadGPS("car", 13.758, 100.503, 5.5); // อัพโหลดรอบที่สอง
+
+  // ปิดการเชื่อมต่อเมื่ออัพโหลดเสร็จ
+  await closeConnection();
+}
+
+uploadData();
 
 app.get("/", (req, res) => {
   res.send("Socket.IO server running");
