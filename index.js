d276728ba8ac207ac28c3ef63f63cf49d008a66e
Sirawit Pulketkij <sirawit.pulketkij@gmail.com>
2024-09-30 14:42:45 +0700

    upload to DB

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ mqtt_client.on("error", function (error) {
 
 mqtt_client.on("message", function (topic, message) {
   // called each time a message is received
-  // console.log("Received message:", topic, message.toString());
+  console.log("Received message:", topic, message.toString());
   // io.emit("mqtt-message", { topic, message: message.toString() });
 
   const validJsonString = message.toString().replace(/'/g, '"');
@@ -52,22 +52,26 @@ mqtt_client.on("message", function (topic, message) {
   const jsonObject = JSON.parse(validJsonString);
   console.log(jsonObject);
 
+  let date = new Date();
+
   uploadGyroscope(
-    jsonObject.Gyroscope.x,
-    jsonObject.Gyroscope.y,
-    jsonObject.Gyroscope.z
+    jsonObject.gyroscope.x,
+    jsonObject.gyroscope.y,
+    jsonObject.gyroscope.z,
+    date
   );
   uploadAccelerometer(
-    jsonObject.Accelerometer.x,
-    jsonObject.Accelerometer.y,
-    jsonObject.Accelerometer.z
+    jsonObject.accelerometer.x,
+    jsonObject.accelerometer.y,
+    jsonObject.accelerometer.z,
+    date
   );
 
   io.emit("mqtt-message", { topic, message: jsonObject });
 });
 
 // subscribe to topic 'my/test/topic'
-mqtt_client.subscribe("test/mpu");
+mqtt_client.subscribe("mpu");
 
 // สร้าง InfluxDB client โดยใช้ environment variables
 const client = new InfluxDBClient({
@@ -75,22 +79,37 @@ const client = new InfluxDBClient({
   token: process.env.INFLUXDB_TOKEN,
 });
 
-async function uploadGyroscope(x, y, z) {
+// async function uploadValue(s, x, y, z, date) {
+//   let val = Math.sqrt(x * x + y * y + z * z);
+//   const ValPoint = Point.measurement(s)
+//     .setFloatField("val", val)
+//     .setTimestamp(date);
+
+//   await client.write(ValPoint, "wireless");
+// }
+
+async function uploadGyroscope(x, y, z, date) {
+  let magnitude = Math.sqrt(x * x + y * y + z * z);
+
   const gyroscopePoint = Point.measurement("gyroscope")
     .setFloatField("x", x)
     .setFloatField("y", y)
     .setFloatField("z", z)
-    .setTimestamp(new Date());
+    .setFloatField("magnitude", magnitude)
+    .setTimestamp(date);
 
   await client.write(gyroscopePoint, "wireless");
 }
 
-async function uploadAccelerometer(x, y, z) {
+async function uploadAccelerometer(x, y, z, date) {
+  let magnitude = Math.sqrt(x * x + y * y + z * z);
+
   const accelerometerPoint = Point.measurement("accelerometer")
     .setFloatField("x", x)
     .setFloatField("y", y)
     .setFloatField("z", z)
-    .setTimestamp(new Date());
+    .setFloatField("magnitude", magnitude)
+    .setTimestamp(date);
 
   await client.write(accelerometerPoint, "wireless");
 }
@@ -120,6 +139,25 @@ async function uploadGPS(latitude, longitude, altitude) {
 
 app.get("/", (req, res) => {
   res.send("Socket.IO server running");
+});
+
+app.get("/data", async (req, res) => {
+  const query = "";
+
+  try {
+    const rows = await client.query(query, "wireless");
+    const formattedRows = rows.map((row) => ({
+      time: row._time,
+      x: row.x,
+      y: row.y,
+      z: row.z,
+    }));
+
+    res.json(formattedRows); // Send the data as a JSON response
+  } catch (error) {
+    console.error("Error querying data:", error);
+    res.status(500).send("Error querying data");
+  }
 });
 
 // Socket.IO connection handling
